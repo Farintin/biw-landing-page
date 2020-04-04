998980358d2107c18f089a5d96c207dc3e6b4328
farintin <farintinobialor@gmail.com>
2020-04-04 19:37:53 +0100

    updated jquery ajax error function handler. And added it"s database file form importation

--- js/forms.js
+++ js/forms.js
@@ -258,9 +258,32 @@ jQuery(document).ready(function($) {
           })
 
         },
-        error: function() {
+        error: function(xhr,status,error) {
 
-          activeFormLoader.hide();
+          console.log(error);
+          activeFormLoaderRemover();
+
+          let formTitle = '#'.concat(activeForm.find('input[type="hidden"]').val());
+          if (formTitle == '#masterclass') {
+
+            $(formTitle).find('.bottom').html("<p>You are just one step away from joining our masterclasses. "
+            + "We'll be in touch will you for the second registration. " + 
+            "A connection error occured in our mail server, please we'll send you an e-mail message later, thank you.</p>");
+
+          } else if (formTitle == '#event') {
+
+            $(formTitle).find('.bottom').html(`<p>Thank's for writing to us your message. 
+              We will try to be responsive as possible. A connection error occured in our mail server, 
+              Our team will get back to you with answer to your message later, thank you.</p>`);
+
+          } else if (formTitle == '#newsletter') {
+
+            $(formTitle).find('.bottom').html(`<p>Thank's for subscribing to our newsletter you will be recieving events info and news feeds on all around our industry.</p>
+              <p>A connection error occured in our mail server, please we'll send you an e-mail message later, thank you.</p>`);
+            $('#modalNewsletterForm').fadeOut(200)
+
+          };
+          $(formTitle).fadeIn(500)
 
         }
 
