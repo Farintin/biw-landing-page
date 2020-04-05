d8320203a9a7d5f9c8c7e73199b7069480754d81
farintin <farintinobialor@gmail.com>
2020-04-05 19:17:14 +0100

    added back-slashes after __DIR__

--- forms.php
+++ forms.php
@@ -3,9 +3,9 @@
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
-  require __DIR__.'PHPMailer\src\PHPMailer.php';
-  require __DIR__.'PHPMailer\src\SMTP.php';
-  require __DIR__.'PHPMailer\src\Exception.php';
+  require __DIR__.'\PHPMailer\src\PHPMailer.php';
+  require __DIR__.'\PHPMailer\src\SMTP.php';
+  require __DIR__.'\PHPMailer\src\Exception.php';
 
 
   require_once (__DIR__.'/vendor/autoload.php');
