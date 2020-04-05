c4e1c3c2fc1f4d1b2142a71d2e76adb20b587edf
farintin <farintinobialor@gmail.com>
2020-04-05 20:09:50 +0100

    edited

--- forms.php
+++ forms.php
@@ -1,11 +1,10 @@
 <?php
-  //header('Content-type: application/json');
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
-  require(__DIR__.'/PHPMailer/src/PHPMailer.php');
-  require(__DIR__.'/PHPMailer/src/SMTP.php');
-  require(__DIR__.'/PHPMailer/src/Exception.php');
+  require('./PHPMailer/src/PHPMailer.php');
+  require('./PHPMailer/src/SMTP.php');
+  require('./PHPMailer/src/Exception.php');
 
 
   require_once (__DIR__.'/vendor/autoload.php');
