f3fde4c8ab78d3635726b020115dc0042840fca7
farintin <farintinobialor@gmail.com>
2020-04-05 19:11:26 +0100

    add __DIR__ in forms.php require dependencies

--- forms.php
+++ forms.php
@@ -3,9 +3,9 @@
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
-  require 'PHPMailer\src\PHPMailer.php';
-  require 'PHPMailer\src\SMTP.php';
-  require 'PHPMailer\src\Exception.php';
+  require __DIR__.'PHPMailer\src\PHPMailer.php';
+  require __DIR__.'PHPMailer\src\SMTP.php';
+  require __DIR__.'PHPMailer\src\Exception.php';
 
 
   require_once (__DIR__.'/vendor/autoload.php');
